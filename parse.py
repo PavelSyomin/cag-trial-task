661b1793fa4c3155531437659f6e73fb008d4edd
Pavel Syomin <pas.ntg@gmail.com>
2021-03-13 18:38:08 +0500

    Refactor code and start adding error handling

--- parse.py
+++ parse.py
@@ -2,6 +2,7 @@ import psycopg2
 import psycopg2.extras as pe
 import xmlschema
 import os
+from datetime import date
 from bs4 import BeautifulSoup
 
 
@@ -43,12 +44,15 @@ def process_file(file):
     with open(file, mode="r", encoding="utf8") as f:
         soup = BeautifulSoup(f, "xml")
 
-    documents = soup("Документ")
+    data = {"receivers": {},
+            "providers": {},
+            "support_measures": [],
+            "support_kinds": {}}
 
-    receivers = {}
-    providers = {}
-    support_measures = []
-    support_kinds = {}
+    documents = soup.find_all("Документ")
+    if documents is None:
+        print("File {0} doesn't have a «Документ» element.".format(file))
+        return False
 
     # Dictionaries to convert numeric codes from the xml file to labels of enumeration types in the database.
     receiver_kinds = {1: "ul", 2: "fl", 3: "npd"}
@@ -58,89 +62,168 @@ def process_file(file):
     bool_codes = {1: True, 2: False}
 
     for document in documents:
-        fl = document("СвФЛ")
-        ul = document("СвЮЛ")
-        provided_support = document("СвПредПод")
+        doc_id = document.get("ИдДок", "")
+        individual = document.find("СвФЛ")
+        organization = document.find("СвЮЛ")
+        provided_support = document.find_all("СвПредПод")
 
-        if len(fl) == 1:
-            fl = fl[0]
-            receiver_tin = fl["ИННФЛ"]
-            name_element = fl.find("ФИО")
-            first_name = name_element["Имя"]
-            surname = name_element["Фамилия"]
-            patronymic = name_element.get("Отчество")
-            if patronymic is None:
-                patronymic = ""
-            receiver_name = " ".join([surname, first_name, patronymic])
+        receiver_tin = ""
+        receiver_name = ""
 
-        if len(ul) == 1:
-            ul = ul[0]
-            receiver_tin = ul["ИННЮЛ"]
-            receiver_name = ul["НаимОрг"]
+        if individual is not None:
+            receiver_tin = individual.get("ИННФЛ", "")
+            if receiver_tin == "":
+                print("ERROR: Document {0} in file {1} doesn't have «ИННФЛ» attribute and will be skipped.".format(doc_id, file))
+                continue
+            name_element = individual.find("ФИО")
+            if name_element is None:
+                print("WARNING: Document {0} in file {1} doesn't have «ФИО» element.".format(doc_id, file))
+            else:
+                first_name = name_element.get("Имя", "")
+                surname = name_element.get("Фамилия", "")
+                patronymic = name_element.get("Отчество", "")
+                receiver_name = " ".join([surname, first_name, patronymic]).strip()
+        elif organization is not None:
+            receiver_tin = organization.get("ИННЮЛ", "")
+            if receiver_tin == "":
+                print("ERROR: Document {0} in file {1} doesn't have «ИННЮЛ» attribute and will be skipped.".format(doc_id, file))
+                continue
+            receiver_name = organization.get("НаимОрг", "")
+            if receiver_name == "":
+                print("WARNING: Document {0} in file {1} doesn't have «НаимОрг» element.".format(doc_id, file))
+        else:
+            print("ERROR: Document {0} in file {1} doesn't have information on receiver and will be skipped".format(doc_id, file))
+            continue
 
-        if receiver_tin not in receivers:
-            receivers[receiver_tin] = receiver_name
+        if receiver_tin not in data["receivers"]:
+            data["receivers"][receiver_tin] = receiver_name
+
+        if len(provided_support) == 0:
+            print("ERROR: Document {0} in file {1} has no information on support measures and will be skipped".format(doc_id, file))
+            continue
 
         for support_measure in provided_support:
-            provider_name = support_measure["НаимОрг"]
-            provider_tin = support_measure["ИННЮЛ"]
-            if provider_tin not in providers:
-                providers[provider_tin] = provider_name
+            provider_name = support_measure.get("НаимОрг", "")
+            if provider_name == "":
+                print("WARNING: Document {0} doesn't have «НаимОрг» attribute.".format(doc_id))
+            provider_tin = support_measure.get("ИННЮЛ", "")
+            if provider_tin == "":
+                print("ERROR: No «ИННЮЛ» attribute of «СвПредПод» element in document {0}. This support measure will be skipped.".format(doc_id))
+                continue
+            if provider_tin not in data["providers"]:
+                data["providers"][provider_tin] = provider_name
 
-            receiver_kind_code = int(support_measure["ВидПП"])
-            receiver_kind = receiver_kinds[receiver_kind_code]
-            receiver_category_code = int(support_measure["КатСуб"])
-            receiver_category = receiver_categories[receiver_category_code]
-            period = support_measure["СрокПод"].replace(".", "/")
-            start_date = support_measure["ДатаПрин"].replace(".", "/")
-            end_date = support_measure.get("ДатаПрекр")
+            receiver_kind_code = int(support_measure.get("ВидПП", ""))
+            if receiver_kind_code == "":
+                print("WARNING: no «ВидПП» attribute.")
+                receiver_kind = None
+            else:
+                receiver_kind = receiver_kinds[receiver_kind_code]
+            receiver_category_code = int(support_measure.get("КатСуб", ""))
+            if receiver_category_code == "":
+                print("WARNING: no «КатСуб» attribute.")
+                receiver_category = None
+            else:
+                receiver_category = receiver_categories[receiver_category_code]
+
+            def string_to_date(string):
+                parts = string.split(".")
+                if len(parts) == 3:
+                    return date(year=parts[2], month=parts[1], day=parts[0])
+                else:
+                    return None
+
+            period = support_measure.get("СрокПод", "00.00.0000")
+            period = string_to_date(period)
+            if period is None:
+                period = date(1, 1, 1)
+            start_date = support_measure.get("ДатаПрин", "00.00.0000")
+            start_date = string_to_date(start_date)
+            if start_date is None:
+                start_date = date (1, 1, 1)
+            end_date = support_measure.get("ДатаПрекр", None)
             if end_date is not None:
-                end_date = end_date.replace(".", "/")
-            form = support_measure.find("ФормПод")["КодФорм"]
-            kind_code = support_measure.find("ВидПод")["КодВид"]
-            kind_name = support_measure.find("ВидПод")["НаимВид"]
-            if kind_code not in support_kinds:
-                support_kinds[kind_code] = kind_name
-            violation = bool_codes[int(support_measure.find("ИнфНаруш")["ИнфНаруш"])]
-            misuse = bool_codes[int(support_measure.find("ИнфНаруш")["ИнфНецел"])]
+                end_date = string_to_date(end_date)
+            form = support_measure.find("ФормПод").get("КодФорм", "0000")
+            if form not in ["0000", "0100", "0200", "0300", "0400", "0500", "0600"]:
+                form = "0000"
+            kind_code = support_measure.find("ВидПод").get("КодВид")
+            kind_name = support_measure.find("ВидПод").get("НаимВид")
+            if kind_code not in data["support_kinds"]:
+                data["support_kinds"][kind_code] = kind_name
+            violations_element = support_measure.find("ИнфНаруш")
+            if violations_element is not None:
+                violation = bool_codes[int(violations_element.get("ИнфНаруш", "2"))]
+                misuse = bool_codes[int(violations_element.get("ИнфНецел", "2"))]
+            else:
+                violation, misuse = False, False
             sizes = support_measure.find_all("РазмПод")
+            if len(sizes) == 0:
+                size = 0.0
+                unit = "unit"
+                data["support_measures"].append((period, start_date, end_date,
+                                         size, unit, violation, misuse,
+                                         receiver_kind, receiver_category,
+                                         receiver_tin, provider_tin,
+                                         kind_code, form))
             for item in sizes:
-                size = item["РазмПод"]
-                unit_code = int(item["ЕдПод"])
+                size = item.get("РазмПод", 0.0)
+                unit_code = int(item.get("ЕдПод", 5))
                 unit = size_units[unit_code]
-                support_measures.append((period, start_date, end_date,
+                data["support_measures"].append((period, start_date, end_date,
                                          size, unit, violation, misuse,
                                          receiver_kind, receiver_category,
                                          receiver_tin, provider_tin,
                                          kind_code, form))
 
-    return receivers, providers, support_measures, support_kinds
+    return data
 
 
 # Function to process xml files in a directory.
-# Gets a path to a directory and a number of files to process (default number is 10).
+# Gets:
+# - a path to a directory,
+# - a number of file to start (default is 0),
+# - a number of file to end (default is 10).
 # Returns 0 on success.
-def process_dir(path, n_files=10):
+def process_dir(path, start=0, end=10):
+    # Connect to the database and get a cursor.
     connection_args = {"database": "smb_support",
                        "user": "pavel",
                        "password": 12345,
                        "host": "localhost",
                        "port": 5432}
     connection = psycopg2.connect(**connection_args)
+    cursor = connection.cursor()
 
     files = os.listdir(path)
 
-    in_receivers = set()
-    in_providers = set()
-    in_support_kinds = set()
+    def get_pk_set(column, table):
+        query = "SELECT {0} FROM {1}".format(column, table)
+        cursor.execute(query)
+        query_result = cursor.fetchall()
+        values_set = {i[0] for i in query_result}
+        return values_set
 
-    for file in files[:n_files]:
+    in_receivers = get_pk_set("tin", "receivers")
+    in_providers = get_pk_set("tin", "providers")
+    in_support_kinds = get_pk_set("code", "support_kinds")
+
+    for i, file in enumerate(files[start:end]):
         if ".xml" not in file:
             continue
 
-        print("File", file, "is being processed")
+        print("Start processing file # {0}: {1}".format(i, file))
 
-        r, p, s, k = process_file(path + "/" + file)
+        d, e = process_file(path + "/" + file)
+        if len(e) > 0:
+            print(*e)
+        r = d["receivers"].copy()
+        p = d["providers"].copy()
+        s = d["support_measures"].copy()
+        k = d["support_kinds"].copy()
+
+        if len(s) == 0:
+            continue
 
         def check_and_update(existing, new):
             for key in existing:
@@ -158,15 +241,14 @@ def process_dir(path, n_files=10):
         p = list(p.items())
         k = list(k.items())
 
-        cursor = connection.cursor()
         sql = {"receivers": "INSERT INTO receivers VALUES %s",
-               "gov_bodies": "INSERT INTO providers VALUES %s",
+               "providers": "INSERT INTO providers VALUES %s",
                "kinds": "INSERT INTO support_kinds VALUES %s",
-               "aid": "INSERT INTO support_measures (period, start_date, end_date, size, size_unit, violation, misuse, receiver_kind, receiver_category, receiver, provider, kind, form) VALUES %s"}
+               "measures": "INSERT INTO support_measures (period, start_date, end_date, size, size_unit, violation, misuse, receiver_kind, receiver_category, receiver, provider, kind, form) VALUES %s"}
         pe.execute_values(cursor, sql["receivers"], r, template=None, page_size=100)
-        pe.execute_values(cursor, sql["gov_bodies"], p, template=None, page_size=100)
+        pe.execute_values(cursor, sql["providers"], p, template=None, page_size=100)
         pe.execute_values(cursor, sql["kinds"], k, template=None, page_size=100)
-        pe.execute_values(cursor, sql["aid"], s, template=None, page_size=100)
+        pe.execute_values(cursor, sql["measures"], s, template=None, page_size=100)
         connection.commit()
 
     connection.close()
@@ -175,5 +257,4 @@ def process_dir(path, n_files=10):
 
 # All files are not valid: 'ВидСуб' attribute not allowed for element.
 # print(validate("data"))
-process_dir("data", 200)
-
+process_dir("data", 800, None)
